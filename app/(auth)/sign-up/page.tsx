9f2308305430cfd1baeb7eb3e4154c37920a2b6b
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2024-05-20 23:04:23 +0530

    project structure

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Sign Up</div>
+  )
+}
+
+export default Page
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="text-5xl font-bold text-zinc-700">
-      Hello world
-    </div>
-  )
-}
