849c6b5fde47c28057b08c1d66c95759d6d1462a
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2024-05-21 12:09:26 +0530

    center dashboard completd,  📦 packages : chart.js,react-chartjs-2,react-countup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,18 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const page = () => {
+  const loggedIn = { firstName: "Harshal Kahar" };
   return (
-    <div>Home Page</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || 'Guest'} subtext="Access and Manage your account & transactions efficienty." />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1234.45} />
+        </header>
+      </div>
+    </section>
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,14 @@
+'use client';
+import CountUp from 'react-countup';
+
+import React from 'react'
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+    return (
+        <div className='w-full'>
+            <CountUp decimals={2} prefix='$' end={amount}></CountUp>
+        </div>
+    )
+}
+
+export default AnimatedCounter
