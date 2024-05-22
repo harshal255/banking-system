e677b771a73126f313737d6a587f9a2d54f4bee5
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2024-05-22 13:55:14 +0530

    user logging functionality completed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -55,8 +55,8 @@ const AuthForm = ({ type }: { type: string }) => {
                 setUser(newUser);
             }
             if (type === 'sign-in') {
-                // const response = await signIn({ email: values.email, password: values.password });
-                // if(response) router.push('/');
+                const response = await signIn({ email: values.email, password: values.password });
+                if (response) router.push('/');
             }
         } catch (error) {
             console.log(error);
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,9 +4,11 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 
-export const signIn = async () => {
+export const signIn = async ({ email, password }: signInProps) => {
   try {
-    //mutation /database/ fetch
+    const { account } = await createAdminClient();
+    const response = await account.createEmailPasswordSession(email, password);
+    return parseStringify(response);
   } catch (error) {
     console.error("Error", error);
   }
